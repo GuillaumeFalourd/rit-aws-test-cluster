88dfe19b74cad1ff82cd7b968bad9a060fb6b513
Guillaume Falourd <guillaume.falourd@zup.com.br>
2021-03-04 16:29:22 -0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -13,7 +13,7 @@ terraform {
 	kubernetes = "~> 1.11.0"
 	local      = "1.4.0"
 	template   = "2.1.2"
-	helm       = "0.10.4"
+	helm       = "0.10.6"
 	external   = "1.2.0"
 	tls        = "2.1.1"
 	archive    = "1.3.0"
