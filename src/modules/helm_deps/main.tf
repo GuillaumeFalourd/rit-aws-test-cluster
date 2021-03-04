f19caa97e304cbaa48e866054a9e8c6ad83a7b3e
Guillaume Falourd <guillaume.falourd@zup.com.br>
2021-03-04 14:12:41 -0300

    Update main.tf

--- src/modules/helm_deps/main.tf
+++ src/modules/helm_deps/main.tf
@@ -1,13 +1,13 @@
 
-# data "helm_repository" "stable" {
-#   name = "stable"
-#   url  = "https://charts.helm.sh/stable"
-# }
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://charts.helm.sh/stable"
+}
 
-# data "helm_repository" "incubator" {
-#   name = "incubator"
-#   url  = "https://charts.helm.sh/incubator"
-# }
+data "helm_repository" "incubator" {
+  name = "incubator"
+  url  = "https://charts.helm.sh/incubator"
+}
 
 # data "helm_repository" "stable" {
 #   name = "stable"
